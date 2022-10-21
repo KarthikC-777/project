0b1a851b65265bcba82775c1448f11d7f7a8c9e5
Smruthirp <56838121+Smruthirp@users.noreply.github.com>
2022-10-21 13:23:05 +0530

    Added leaveSchema

---
+++ src/user/leave.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Date } from "mongoose";
+
+export type leaveDocument = leave & Document;
+
+@Schema()
+export class leave {
+    @Prop({})
+    email: string;
+
+    @Prop({required:true})
+    leaveDate: string;
+
+    @Prop({default: "Pending"})
+    status: string;
+}
+
+export const leaveschema = SchemaFactory.createForClass(leave);
